d88559331af2e467cfdd2d52756cc12a10bf0076
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-28 14:44:23 +0100

    feat: add all routes for this project

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
-
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ChatbotController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthenticatedSessionController::class, 'register']);
+Route::post('/login', [AuthenticatedSessionController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/chatbot/chat', [ChatbotController::class, 'chat'])->name('chat');
-    Route::get('/chat/history', [ChatbotController::class, 'showChatHistory'])->name('chat.history');
-    Route::get('/chat/history/{sessionId}', [ChatbotController::class, 'showChatSessionHistory'])->name('chat.session.history');
-    Route::delete('/chat/history/{sessionId}', [ChatbotController::class, 'deleteChatHistory'])->name('chat.history.delete');
+    Route::post('/chat', [ChatbotController::class, 'chat'])->name('chat');
+    Route::get('/logout', [AuthenticatedSessionController::class, 'logout'])->name('logout');
 });
